5e77b4eeacd5513123f301627e1c85a0945149da
Jordan <jordan@chude.co>
2021-05-27 09:52:54 -0400

    anonymous auth

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
         <h3>Home</h3>
+        <Login/>
     </div>
 </template>
+
+<script>
+// IMPORT LOGIN COMPONENT
+import Login from './Login'
+
+// REGISTERING COMPONENT
+export default {
+    components: {
+        Login
+    }
+}
+
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <aside>
+        <h3>Sign in anonymously</h3>
+        <button @click="auth.signInAnonymously()">Sign in</button>
+    </aside>
+</template>
+
+<script>
+import { auth } from './firebase'
+
+export default {
+    data() {
+        return {
+            auth,
+        }
+    },
+}
+
+</script>
