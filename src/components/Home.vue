eae1a35d3eb12603c6a5a1c9596ff70d92a7d59b
Jordan <jordan@chude.co>
2021-05-28 09:53:52 -0400

    realtime auth state

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
         <h3>Home</h3>
-        <Login/>
+        <User v-slot:user="{ user }">
+
+            <div v-if="user">
+                <p>Logged-in as {{user.uid}} </p>
+            </div>
+            <Login v-else/>
+
+        </User>
     </div>
 </template>
 
 <script>
 // IMPORT LOGIN COMPONENT
 import Login from './Login'
+import User from './User.vue'
 
 // REGISTERING COMPONENT
 export default {
     components: {
-        Login
+        Login,
+        User
     }
 }
 
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,15 +1,40 @@
 <template>
-    
+    <div>
+        <!-- SLOT PROP
+        MAKES USER DATA AVAILABLE TO OTHER COMPONENTS
+        THAT CONSUMES USER COMPONENT -->
+        <slot name="user" v-bind:user="user"></slot>
+    </div>
 </template>
 
 <script>
+// IMPORT AUTH
+import { auth } from "./firebase"
+
 // IMPORT VUE COMPOSITION API
 import { ref } from '@vue/composition-api'
 
 export default {
     setup() {
+        // DEFAULT VALUE NULL
+        // USER NOT SIGNED IN
+        const user = ref(null);
 
+        // UNSUBSCRIBE FROM STREAM WHEN NEEDED
+        // AVOIDS MEMORY LEAKS IN APPLICATION
+        const unsubscribe = auth.onAuthStateChanged(
+            firebaseUser => user.value = firebaseUser
+        );
+        return {
+            user,
+            unsubscribe,
+        }
     },
+
+    // WHEN COMPONENT IS DESTROYED, UNSUBSCRIBE
+    destroyed() {
+        this.unsubscribe()
+    }
 }
 
 </script>
