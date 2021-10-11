4227c91987e38424cba5f2a898af8d305faccce1
Jordan <jordan@chude.co>
2021-10-10 22:04:15 -0400

    chatroom named slots fixed

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -2,7 +2,7 @@
     <main>
         <h3>Welcome to Chatroom {{ chatId }} </h3>
         <User>
-            <template>
+            <template #user = "{ user }">
             <div v-if="user">
                 <ul>
                     <li v-for="message of messages" :key="message.id">
